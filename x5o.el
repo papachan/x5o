1d58ea4bdb5fc456e0ce21a3d77d1a42f2e0f677
Dan Loaiza <papachan@gmail.com>
2015-12-20 20:54:29 -0500

    descriptive file

--- x5o.el
+++ x5o.el
@@ -1,15 +1,36 @@
-;;; x5o.el --- 
-;;;
-;; Copyright (C) 2015 Dan Loaiza <papachan@gmail.com>
-
+;;; x5o.el --- a bullshit lisp plugin
+;; Copyright (C) 2015 Dan Loaiza
 
 ;; Author: Dan Loaiza <papachan@gmail.com>
-;; Maintainer: 
+;; Maintainer: Dan Loaiza <papachan@gmail.com>
+;; Created: Feb 8 2015
 ;; Version: 0.0.1
 ;; URL: https://github.com/papachan/x5o.el
+;; Keywords: x5o
+;; Compatibility: GNU Emacs: 24.x
+
+;; This file is not part of GNU Emacs, but it is distributed under
+;; the same terms as GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Le Papachan <papachan@gmail.com>
 
 ;;; Code:
-
+(eval-when-compile (require 'cl))
 
 (defun print-hello ()
   "hello")
